eb848839918562fe5e1ff4bc19cb0e2064868d36
Xenymor <101249409+Xenymor@users.noreply.github.com>
2023-09-29 13:05:41 +0200

    Backup

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,7 +12,6 @@ public class MyBot : IChessBot
     int[] piecePhase = { 0, 0, 1, 1, 2, 4, 0 };
     ulong[] psts = { 657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902 };
     Move bestRootMove = Move.NullMove;
-    TTEntry[] transTable = new TTEntry[ENTRIES];
     int foundCounter; //#DEBUG
     int notFoundCounter; //#DEBUG
     int returnCounter; //#DEBUG
@@ -37,7 +36,7 @@ public class MyBot : IChessBot
 
     public MyBot()
     {
-        scorePool = new int[80][];
+        scorePool = new int[79][];
         for (int i = 0; i < scorePool.Length; i++)
         {
             scorePool[i] = new int[270];
@@ -57,15 +56,19 @@ public class MyBot : IChessBot
         int alpha = eval - 25;
         int beta = eval + 25;
         int depth = 3;
-        while (depth < 50) {
+        while (depth <= 39) {
             eval = alphaBeta(alpha, beta, depth, true, timer);
             if (eval <= alpha)
                 alpha -= 50;
             else if (eval >= beta)
                 beta += 50;
             else
+            {
                 depth++;
-            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining/30)
+                alpha = eval - 25;
+                beta = eval + 25;
+            }
+            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining/50)
             {
                 eval = lastEval; //#DEBUG
                 break;
@@ -99,8 +102,9 @@ public class MyBot : IChessBot
             if (bestScore >= beta) return bestScore;
             alpha = Math.Max(alpha, bestScore);
         }
-
-        int[] scores = scorePool[depth + 40];
+        if (depth + 39 < 0 || depth + 39 > scorePool.Length - 1) //#DEBUG
+            Console.WriteLine("Error: Depth: " + depth); //#DEBUG
+        int[] scores = scorePool[depth + 39];
 
         TTEntry ttEntry = transpositionTable[board.ZobristKey % ENTRIES];
         ushort moveHash;
@@ -108,11 +112,11 @@ public class MyBot : IChessBot
         {
             foundCounter++; //#DEBUG
             if (!isFirstCall && ttEntry.depth >= depth
-                && ((ttEntry.flag == 2 && ttEntry.eval >= beta)
-                    || (ttEntry.flag == 1 && ttEntry.eval <= alpha)
-                    || (ttEntry.flag == 3)))
+                && ((ttEntry.flag == 1 && ttEntry.eval >= beta)
+                    || (ttEntry.flag == 0 && ttEntry.eval <= alpha) 
+                    || (ttEntry.flag == 2)))
             {
-                returnCounter++;
+                returnCounter++; //#DEBUG
                 return ttEntry.eval;
             }
             moveHash = ttEntry.move;
@@ -132,7 +136,7 @@ public class MyBot : IChessBot
         int originalAlpha = alpha;
         for (byte i = 0; i < moves.Length; i++)
         {
-            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30)
+            if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 40)
                 return 30000;
 
             for (int j = i + 1; j < moves.Length; j++)
@@ -164,7 +168,7 @@ public class MyBot : IChessBot
                     break;
             }
         }
-        byte flag = (byte)(bestScore >= beta ? 2 : (bestScore > originalAlpha ? 3 : 1));
+        byte flag = (byte)(bestScore >= beta ? 1 : (bestScore > originalAlpha ? 2 : 0));
         if (depth >= ttEntry.depth || ttEntry.key != board.ZobristKey)
             transpositionTable[board.ZobristKey % ENTRIES] = new TTEntry(board.ZobristKey, bestMove.RawValue, bestScore, (byte)depth, flag);
         return bestScore;
